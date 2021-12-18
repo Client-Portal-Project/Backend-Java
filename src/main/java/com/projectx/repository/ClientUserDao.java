7dfdebca7c21fd49acdb48aca2bd1ee8aec2dd67
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-12-17 22:08:43 -0500

    ClientUser Controller init

--- src/main/java/com/projectx/repository/ClientUserDao.java
+++ src/main/java/com/projectx/repository/ClientUserDao.java
@@ -1,12 +1,18 @@
 package com.projectx.repository;
 
-import com.projectx.model.ClientUser;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.projectx.model.Client;
+import com.projectx.model.ClientUser;
+import com.projectx.model.User;
+
 @Repository("clientUserDao")
 @Transactional
 public interface ClientUserDao extends JpaRepository<ClientUser, Integer> {
-
+	ClientUser findClientUserByUser(User user);
+	List<ClientUser> findClientUserByClient(Client client);
 }
