d36c80360209d1466ecd6935aa20afe8e7504197
gabrielgil245 <gabrielgil245@gmail.com>
2021-12-20 12:15:20 -0600

    added user controller test file

---
+++ src/test/java/com/projectx/controller/UserControllerTest.java
@@ -0,0 +1,38 @@
+package com.projectx.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void editUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
