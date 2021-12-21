0aef1c191174b0d04aef26d97c505cd6144e460c
gabrielgil245 <gabrielgil245@gmail.com>
2021-12-21 11:50:19 -0600

    user service test file added

--- src/test/java/com/projectx/service/UserServiceTest.java
+++ src/test/java/com/projectx/service/UserServiceTest.java
@@ -1,4 +1,38 @@
 package com.projectx.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class UserServiceTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+
+    @Test
+    void findUserByEmail() {
+    }
+
+    @Test
+    void findAllUsers() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUserByEmailAndPassword() {
+    }
+
+    @Test
+    void editUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
 }
