d07d529566bb4d5ba343db17e8dc8ce7e482492a
April W <april.weaver@revature.net>
2022-02-11 12:35:11 -0500

    finished controller, need javadocs

--- src/main/java/com/projectx/controllers/ApplicationController.java
+++ src/main/java/com/projectx/controllers/ApplicationController.java
@@ -1,12 +1,17 @@
 package com.projectx.controllers;
 
 import com.projectx.Driver;
+import com.projectx.models.Applicant;
+import com.projectx.models.ApplicantOccupation;
 import com.projectx.models.Application;
+import com.projectx.models.Need;
 import com.projectx.services.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("applicationController")
 @RequestMapping(value = "application")
@@ -56,5 +61,48 @@ public class ApplicationController {
         } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    @GetMapping("{id}")
+    public ResponseEntity<Application> getApplication(@PathVariable int id) {
+        Application temp = applicationService.getApplication(id);
+        if (temp != null) {
+            return new ResponseEntity<>(temp, HttpStatus.FOUND);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * @param applicant
+     * @return
+     */
+    @GetMapping("applicant")
+    public ResponseEntity<List<Application>> getApplicationByApplicant(@RequestBody Applicant applicant) {
+        return new ResponseEntity<>(applicationService.getAllApplicationsByApplicant(applicant), HttpStatus.OK);
+    }
+
+    /**
+     * @param applicantOccupation
+     * @return
+     */
+    @GetMapping("occupation")
+    public ResponseEntity<List<Application>> getApplicationByOccupation(@RequestBody ApplicantOccupation
+                                                                                    applicantOccupation) {
+        return new ResponseEntity<>(applicationService
+                .getAllApplicationsByApplicantOccupation(applicantOccupation), HttpStatus.OK);
+    }
+
+    /**
+     * @param need
+     * @return
+     */
+    @GetMapping("need")
+    public ResponseEntity<List<Application>> getApplicationByNeed(@RequestBody Need need) {
+        return new ResponseEntity<>(applicationService.getAllApplicationsByNeed(need), HttpStatus.OK);
     }
 }
