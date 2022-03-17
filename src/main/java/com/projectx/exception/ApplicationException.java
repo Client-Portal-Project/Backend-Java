d4bdddc80934ea3c01bb46f43d7951bc9574ee59
Steven Hanley <Steven.Hanley@revature.net>
2022-03-17 15:42:10 -0500

    trying to fix jenkins pipe

--- src/main/java/com/projectx/exception/ApplicationException.java
+++ src/main/java/com/projectx/exception/ApplicationException.java
@@ -1,14 +1,15 @@
 package com.projectx.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-@Getter
+@Data
 @AllArgsConstructor
-public class ApplicationException{
+public class ApplicationException extends Exception{
     private String message;
     private Throwable e;
     private HttpStatus httpStatus;
