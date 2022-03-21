dbe8179ed555d6c60f93fe39c301c4979175c8dc
Steven Hanley <Steven.Hanley@revature.net>
2022-03-21 15:13:41 -0500

    Code smell requirement changed. Just printing out fields in exception handler to see if I get another curve ball when smells are resolved.

--- src/main/java/com/projectx/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/projectx/exception/ApplicationExceptionHandler.java
@@ -1,20 +1,15 @@
 package com.projectx.exception;
 
-import com.projectx.models.Application;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.sql.SQLOutput;
-import java.time.ZonedDateTime;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
     @ExceptionHandler(value = {ApplicationRequestException.class})
     public ResponseEntity<Object> handleApplicationRequestException(ApplicationRequestException e) {
-        System.out.println(e.getHttpStatus() + " : " + e.getTimestamp());
         return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/projectx/exception/ApplicationRequestException.java
+++ src/main/java/com/projectx/exception/ApplicationRequestException.java
@@ -9,19 +9,12 @@ import java.time.ZonedDateTime;
 @Data
 public class ApplicationRequestException extends RuntimeException{
 
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime timestamp;
-
     public ApplicationRequestException(String message){
         super(message);
-        this.httpStatus = HttpStatus.BAD_REQUEST;
-        this.timestamp = ZonedDateTime.now();
     }
 
     public ApplicationRequestException(String message, Throwable cause){
         super(message, cause);
-        this.httpStatus = HttpStatus.BAD_REQUEST;
-        this.timestamp = ZonedDateTime.now();
     }
 
 }
