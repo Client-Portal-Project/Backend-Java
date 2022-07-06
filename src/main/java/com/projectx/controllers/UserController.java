9ff5df1b0f23ac4f70d3bbc2d8cf4fe8bce24c09
Hiroyuki0487 <Hiroyuki0487@users.noreply.github.com>
2022-07-06 16:47:06 -0400

    ung 409 and 415 errors

--- src/main/java/com/projectx/controllers/UserController.java
+++ src/main/java/com/projectx/controllers/UserController.java
@@ -1,22 +1,33 @@
 package com.projectx.controllers;
 
+import java.util.List;
+import java.util.Objects;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.projectx.Driver;
 import com.projectx.aspects.annotations.NoAuth;
 import com.projectx.models.User;
 import com.projectx.services.UserService;
 import com.projectx.utility.JwtUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.Objects;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController("userController")
-@RequestMapping("user")
+@RequestMapping(value="user",consumes=MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin(value = Driver.CROSS_ORIGIN_VALUE, allowCredentials = "true")
 public class UserController {
     @Autowired
--- src/main/java/com/projectx/models/User.java
+++ src/main/java/com/projectx/models/User.java
@@ -1,10 +1,18 @@
 package com.projectx.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 /*
     Will handle the User's information and functionality
@@ -16,6 +24,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
+//@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
