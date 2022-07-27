03ea78dd0fe384b721ee77cd9e8d64f786f22a86
18xxperson <46502267+18xxperson@users.noreply.github.com>
2022-07-27 10:21:18 -0700

    Update UserController.java

--- src/main/java/com/projectx/controllers/UserController.java
+++ src/main/java/com/projectx/controllers/UserController.java
@@ -1,17 +1,10 @@
 package com.projectx.controllers;
 
-import java.util.List;
-import java.util.Objects;
-
-import javax.mail.*;
-import javax.servlet.http.HttpServletRequest;
-
 import com.projectx.Driver;
 import com.projectx.aspects.annotations.NoAuth;
 import com.projectx.models.User;
 import com.projectx.services.UserService;
 import com.projectx.utility.JwtUtil;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,21 +12,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.*;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController("userController")
