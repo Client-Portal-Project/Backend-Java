7c1c3790984b111f6899adec2a61c829345e97d1
18xxperson <46502267+18xxperson@users.noreply.github.com>
2022-07-28 16:12:32 -0700

    Merge branch 'main' of https://github.com/Client-Portal-Project/Backend-Java

--- src/main/java/com/projectx/controllers/UserController.java
+++ src/main/java/com/projectx/controllers/UserController.java
@@ -1,9 +1,13 @@
 package com.projectx.controllers;
 
+import java.net.Authenticator;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.websocket.Session;
 
 import com.projectx.Driver;
 import com.projectx.aspects.annotations.NoAuth;
@@ -11,6 +15,7 @@ import com.projectx.models.User;
 import com.projectx.services.UserService;
 import com.projectx.utility.JwtUtil;
 
+import org.aspectj.bridge.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -149,5 +154,35 @@ public class UserController {
         }
 
         return response;
+    }
+
+
+    public void sendEmail(String email,String subject,String message)
+    {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port",587);
+        props.put("mail.smtp.auth",true);
+        props.put("mail.smtp.starttls.enable",true);
+        props.put("mail.smtp.ssl.protocols","TLSv1.2");
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                //The password must be an app specific password with the gmail smtp server
+                return new PasswordAuthentication("18xxperson@gmail.com","ovilmpbewocdmwjz");
+            }
+        });
+        try {
+            MimeMessage msg = new MimeMessage(session);
+            msg.setFrom();
+            msg.setRecipients(Message.RecipientType.TO,
+                    email);
+            msg.setSubject(subject);
+            msg.setSentDate(new Date());
+            msg.setText(message);
+            Transport.send(msg);
+        } catch (MessagingException mex) {
+            System.out.println("send failed, exception: " + mex);
+        }
     }
 }
