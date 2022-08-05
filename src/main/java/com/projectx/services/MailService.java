f6a6e0abb7c54371921eaef36c7d1cc6484892e8
18xxperson <46502267+18xxperson@users.noreply.github.com>
2022-08-05 13:53:27 -0700

    moving things around

--- src/main/java/com/projectx/services/MailService.java
+++ src/main/java/com/projectx/services/MailService.java
@@ -28,7 +28,6 @@ public class MailService {
         mail.setSenderPassword("ovilmpbewocdmwjz");
         mail.setMessage("Hello "+user.getFirstName()+"Your new password is "+user.getPassword());
         mail.setFromEmail("18xxperson@gmail.com");
-        sendEmail(mail);
         return mail;
     }
 
@@ -44,36 +43,8 @@ public class MailService {
         mail.setSenderPassword("ovilmpbewocdmwjz");
         mail.setMessage("Hello "+user.getFirstName()+"Your new password is "+user.getPassword());
         mail.setFromEmail("18xxperson@gmail.com");
-        sendEmail(mail);
         return mail;
     }
 
-    public void sendEmail(Mail mail)
-    {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port",587);
-        props.put("mail.smtp.auth",true);
-        props.put("mail.smtp.starttls.enable",true);
-        props.put("mail.smtp.ssl.protocols","TLSv1.2");
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                //The password must be an app specific password with the gmail smtp server
-                return new PasswordAuthentication(mail.getFromEmail(),mail.getSenderPassword());
-            }
-        });
-        try {
-            MimeMessage msg = new MimeMessage(session);
-            msg.setFrom();
-            msg.setRecipients(Message.RecipientType.TO,
-                    mail.getSendToEmail());
-            msg.setSubject(mail.getSubject());
-            msg.setSentDate(new Date());
-            msg.setText(mail.getMessage());
-            Transport.send(msg);
-        } catch (MessagingException mex) {
-            System.out.println("send failed, exception: " + mex);
-        }
-    }
+
 }
